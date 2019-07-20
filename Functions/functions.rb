54189182e0e26036f347f52af7c07f6d9612fd32
Lessie Nyambwe <lessienyambwe@Lessies-MacBook-Air.local>
2019-07-20 16:03:02 -0600

    I added a functions folder and created a functions file

---
+++ Functions/functions.rb
@@ -0,0 +1,19 @@
+
+# method
+def hello()
+  p "hello World"
+end
+hello()
+
+# function
+def helloWithFirstName(firstName)
+  p "hello " + firstName
+end
+
+helloWithFirstName("Legendary")
+
+def helloWithFullName(firstName, lastName)
+  p "hello " + firstName + " " + lastName
+end
+
+helloWithFullName("Lessie", "Nyambwe")
